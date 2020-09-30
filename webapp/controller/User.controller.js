75d3cde3697985fa8a306f77948b0a995501efb7
Enzo Schroeder <enzo.schroeder@cpro-ips.com>
2020-09-30 15:43:42 +0200

    initial commit

---
+++ webapp/controller/User.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+	"com/esr/myfirstapplication/controller/BaseController"
+], function (BaseController) {
+	"use strict";
+
+	return BaseController.extend("com.esr.myfirstapplication.controller.User", {
+		onInit: function () {
+			
+		}
+	});
+});
